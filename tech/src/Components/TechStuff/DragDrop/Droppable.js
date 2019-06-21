f1edb0fbad599ad14acdc7a970e850fe88b69025
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-21 13:24:36 +0200

    Feature: added draggable and droppable component

---
+++ tech/src/Components/TechStuff/DragDrop/Droppable.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+
+background-color: #555;
+width: 80%;
+height: 35rem;
+margin: .5rem;
+display: flex;
+flex-wrap: wrap;
+
+`;
+
+
+class Droppable extends React.Component {
+        drop = (event) => {
+                event.preventDefault();
+                const data = event.dataTransfer.getData('transfer');
+                event.target.append(document.getElementById(data));
+              };
+
+              allowDrop = (event) => {
+                event.preventDefault();
+              };
+
+        render() { 
+                return ( 
+                        <StyledDiv id={this.props.id}
+                        onDrop={this.drop}
+                        onDragOver={this.allowDrop} >
+                        {this.props.children}
+                </StyledDiv>   
+                 );
+        }
+}
+ 
+export default Droppable;
